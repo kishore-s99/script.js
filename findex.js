de43434601e77964d3b2b7ddd4cadf132cc42576
kishore-s99 <skishoreBTECH24@ced.alliance.edu.in>
2026-08-06 00:47:53 +0530

    Create findex.js

---
+++ findex.js
@@ -0,0 +1,13 @@
+let str = prompt("Enter a string:");
+
+let count = 0;
+
+for (let i = 0; i < str.length; i++) {
+    let ch = str[i].toLowerCase();
+
+    if (ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u') {
+        count++;
+    }
+}
+
+console.log("Number of vowels: " + count);
