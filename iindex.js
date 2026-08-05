2b021953fa070a449ec9edad98ef32e7ac5cd70b
kishore-s99 <skishoreBTECH24@ced.alliance.edu.in>
2026-08-06 00:51:56 +0530

    Create iindex.js

---
+++ iindex.js
@@ -0,0 +1,16 @@
+let text = prompt("Enter a message:");
+let result = "";
+
+for (let i = 0; i < text.length; i++) {
+    let ch = text[i];
+
+    if (ch >= 'A' && ch <= 'Z') {
+        result += String.fromCharCode(((ch.charCodeAt(0) - 65 + 3) % 26) + 65);
+    } else if (ch >= 'a' && ch <= 'z') {
+        result += String.fromCharCode(((ch.charCodeAt(0) - 97 + 3) % 26) + 97);
+    } else {
+        result += ch;
+    }
+}
+
+console.log("Encrypted Message: " + result);
