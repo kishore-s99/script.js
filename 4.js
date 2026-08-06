5001b83fa54f2dfdc058ea89b1fc01fcd3e78df4
kishore-s99 <skishoreBTECH24@ced.alliance.edu.in>
2026-08-06 08:22:52 +0530

    Update and rename cindex.js to 4.js

--- 4.js
+++ 4.js
@@ -13,3 +13,13 @@ if (c > largest) {
 }
 
 console.log("Largest = " + largest);
+
+Sample runs:
+1. Input: 4, 9, 2
+   Output: Largest = 9
+
+2. Input: 15, 7, 20
+   Output: Largest = 20
+
+3. Input: 3, 3, 1
+   Output: Largest = 3
