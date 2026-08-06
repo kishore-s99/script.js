66872fb860b93184f5e3ebaeefcd5e4677bf1edf
kishore-s99 <skishoreBTECH24@ced.alliance.edu.in>
2026-08-06 08:20:02 +0530

    Update and rename bindex.js to 3.js

--- 3.js
+++ 3.js
@@ -1,7 +1,18 @@
 let num = parseInt(prompt("Enter a number:"));
-
 if (num % 2 === 0) {
     console.log("Even");
 } else {
     console.log("Odd");
 }
+
+
+Sample runs:
+1. Input: 8
+   Output: 8 is Even
+
+2. Input: 13
+   Output: 13 is Odd
+
+3. Input: 0
+   Output: 0 is Even
+
