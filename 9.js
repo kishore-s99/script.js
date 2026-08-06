e7310dfe3abe56e33da5f97fd7f40587954c55fc
kishore-s99 <skishoreBTECH24@ced.alliance.edu.in>
2026-08-06 08:29:26 +0530

    Update and rename hindex.js to 9.js

---
+++ 9.js
@@ -0,0 +1,17 @@
+let num = parseInt(prompt("Enter an integer:"));
+
+num = Math.abs(num);
+
+let count = num.toString().length;
+
+console.log("Number of digits: " + count);
+
+Sample runs:
+1. Input: 7
+   Output: Number of digits = 1
+
+2. Input: 45689
+   Output: Number of digits = 5
+
+3. Input: -1234
+   Output: Number of digits = 4
--- hindex.js
+++
@@ -1,7 +0,0 @@
-let num = parseInt(prompt("Enter an integer:"));
-
-num = Math.abs(num);
-
-let count = num.toString().length;
-
-console.log("Number of digits: " + count);
