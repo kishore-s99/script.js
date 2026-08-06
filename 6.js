3ec70a4cfa825888fdf052d95785403dc0567d01
kishore-s99 <skishoreBTECH24@ced.alliance.edu.in>
2026-08-06 08:25:08 +0530

    Update and rename eindex.js to 6.js

--- 6.js
+++ 6.js
@@ -1,9 +1,17 @@
 let str = prompt("Enter a string:");
-
 let reversed = str.split("").reverse().join("");
-
 if (str === reversed) {
     console.log("Palindrome");
 } else {
     console.log("Not a Palindrome");
 }
+
+Sample runs:
+1. Input: madam
+   Output: madam is a Palindrome
+
+2. Input: level
+   Output: level is a Palindrome
+
+3. Input: hello
+   Output: hello is Not a Palindrome
