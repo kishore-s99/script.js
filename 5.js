2e24d1e4c09ba2f2d093bf433fc0da919eb9cc44
kishore-s99 <skishoreBTECH24@ced.alliance.edu.in>
2026-08-06 08:23:51 +0530

    Update and rename dindex.js to 5.js

---
+++ 5.js
@@ -0,0 +1,13 @@
+let str = prompt("Enter a string:");
+let reversed = str.split("").reverse().join("");
+console.log("Reversed String: " + reversed);
+
+Sample runs:
+1. Input: hello
+   Output: Reversed string = olleh
+
+2. Input: crypto
+   Output: Reversed string = otpyrc
+
+3. Input: JavaScript
+   Output: Reversed string = tpircSavaJ
--- dindex.js
+++
@@ -1,5 +0,0 @@
-let str = prompt("Enter a string:");
-
-let reversed = str.split("").reverse().join("");
-
-console.log("Reversed String: " + reversed);
