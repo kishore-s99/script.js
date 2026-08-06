14678a049af5e0f461d9c452c1730e716d0c1819
kishore-s99 <skishoreBTECH24@ced.alliance.edu.in>
2026-08-06 08:26:52 +0530

    Update and rename findex.js to 7.js

--- 7.js
+++ 7.js
@@ -11,3 +11,14 @@ for (let i = 0; i < str.length; i++) {
 }
 
 console.log("Number of vowels: " + count);
+
+Sample runs:
+1. Input: hello
+   Output: Number of vowels = 2
+
+2. Input: education
+   Output: Number of vowels = 5
+
+3. Input: rhythm
+   Output: Number of vowels = 0
+
